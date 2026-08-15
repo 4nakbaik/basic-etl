f9de92060dc03d7af527aaac2044280fb38e2772
4nakbaik <shoumadono@gmail.com>
2026-08-15 15:14:11 +0700

    etl

--- ETL/etl2.py
+++ ETL/etl2.py
@@ -27,28 +27,37 @@ def transform(customers, products, orders):
         lambda x: re.sub(r'[^a-zA-Z\s]', '', x).strip().title()
     )
     #validasi kolom customer_name min 3 char
-    customers = customers[customers['customer_mame'].str.len() >= 3]
-    customers = customers.drop_duplicate(subset =['customer_id']) #hapus dupe
+    customers = customers[customers['customer_name'].str.len() >= 3]
+    customers = customers.drop_duplicates(subset =['customer_id']) #hapus dupe
 
     #validasi kolom base_price 
     products = products[products['base_price'] > 0]
-    products = products.drop_duplicate(subset = ['product_id']) 
+    products = products.drop_duplicates(subset = ['product_id']) 
 
-    #validasi kolom status transaksi harus dalam kondisi "paid"
+    #filter kolom status transaksi harus dalam kondisi "paid"
     orders = orders[orders['status'] == 'paid']
-    #validasi kolom quantity dan price value tidak boleh < 0
-    orders = orders[orders('quantity') > 0 and orders('price') > 0]
-    orders = orders.drop_duplicate(subset = ['order_id'])
+    #filter kolom quantity dan total price value tidak boleh < 0
+    orders = orders[(orders['quantity'] > 0) & (orders['total_price'] > 0)]
+    orders = orders.drop_duplicates(subset = ['order_id'])
 
     return customers, products, orders
 
-def load():
-    pass
+def load(customers, products, orders):
+    
+    customers.to_sql('dim_customers',engine, if_exists = 'replace', index = False)
+    products.to_sql('dim_products',engine, if_exists = 'replace', index = False)
+    orders.to_sql('fact_orders',engine, if_exists = 'replace', index = False)
+
 
 def main():
-    customers, products, orders = extract()
-    customers, products, orders = transform(customers, products, orders)
-    load()
+    df_customers, df_products, df_orders = extract()
+    cl_customers, cl_products, cl_orders = transform(df_customers, df_products, df_orders)
+    success = load(cl_customers, cl_products, cl_orders)
+
+    if success:
+        print('Load success')
+    else:
+        print('Load error')     
 
 if __name__ == "__main__":
     main()
